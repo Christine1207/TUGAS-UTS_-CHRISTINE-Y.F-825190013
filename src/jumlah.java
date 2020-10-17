411a90b9a064b90afea2437a310e87bd037ed0b7
christine <christineyulianaf12@gmail.com>
2020-10-17 11:48:06 +0700

    Hitung Rata-Rata Nilai

--- src/jumlah.java
+++ src/jumlah.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
 public class jumlah {
-    
+    public static void main (String[] args) {
+        tugas t = new tugas();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Masukkan Nama Mahasiswa : ");
+        t.nama = scan.nextLine();
+
+        System.out.print("Masukkan NIM Mahasiswa : ");
+        t.nim = scan.nextInt();
+
+        System.out.print("Masukkan Nilai Tugas : ");
+        t.nilaiTugas = scan.nextFloat();
+
+        System.out.print("Masukkan Nilai UTS : ");
+        t.nilaiUTS = scan.nextFloat();
+
+        System.out.print("Masukkan Nilai UAS : ");
+        t.nilaiUAS = scan.nextFloat();
+
+        System.out.println("Hasil Rata-Rata : " + t.rataRata(t.nilaiTugas, t.nilaiUTS, t.nilaiUAS));
+    }
+
 }
+
+
--- src/tugas.java
+++ src/tugas.java
@@ -1,2 +1,70 @@
 public class tugas {
+    // class tugas
+    String nama;
+    int nim;
+    float nilaiTugas;
+    float nilaiUTS;
+    float nilaiUAS;
+
+    // constructor
+    public tugas() {
+        this.nama = nama;
+        this.nim = nim;
+        this.nilaiTugas = nilaiTugas;
+        this.nilaiUTS = nilaiUTS;
+        this.nilaiUAS = nilaiUAS;
+    }
+    public tugas(String nama, int nim, float nilaiTugas, float nilaiUTS, float nilaiUAS) {
+        this.nama = nama;
+        this.nim = nim;
+        this.nilaiTugas = nilaiTugas;
+        this.nilaiUTS = nilaiUTS;
+        this.nilaiUAS = nilaiUAS;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getNim() {
+        return nim;
+    }
+
+    public void setNim(int nim) {
+        this.nim = nim;
+    }
+
+    public float getNilaiTugas() {
+        return nilaiTugas;
+    }
+
+    public void setNilaiTugas(float nilaiTugas) {
+        this.nilaiTugas = nilaiTugas;
+    }
+
+    public float getNilaiUTS() {
+        return nilaiUTS;
+    }
+
+    public void setNilaiUTS(float nilaiUTS) {
+        this.nilaiUTS = nilaiUTS;
+    }
+
+    public float getNilaiUAS() {
+        return nilaiUAS;
+    }
+
+    public void setNilaiUAS(float nilaiUAS) {
+        this.nilaiUAS = nilaiUAS;
+    }
+
+    // class method
+    float rataRata(float nilaiTugas, float nilaiUTS, float nilaiUAS){
+        return (nilaiTugas + nilaiUTS + nilaiUAS)/3;
+    }
 }
+
